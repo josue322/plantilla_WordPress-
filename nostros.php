858c7bf7789e9b6f6b12ca53d31ff0a0eae21b5b
Josue López <josuexd123lc@gmail.com>
2024-05-15 11:31:54 -0500

    udapte

---
+++ nostros.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Nosotros</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="MyraStudio" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="plantilla/Admin/vertical/assets/images/favicon.ico">
+    <!-- App css -->
+    <link href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <?php include ('include/menu.php'); ?>
+    <br><br><br>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-8 d-flex justify-content-center align-items-center">
+                <img src="imagenes/33926398-personal-del-equipo-de-ventas-sonriente-en-supermercado-con-los-brazos-cruzados (1).jpg"
+                    class="img-fluid" alt="Imagen de Nosotros">
+            </div>
+            <div class="col-md-4">
+                <h2 class="text-center">Sobre Nosotros</h2>
+                <p class="text-justify large-text">
+                    Bienvenidos a nuestra tienda virtual. Somos una empresa comprometida con ofrecer una experiencia de
+                    compra en línea excepcional.
+                    Contamos con un catálogo extenso de productos de alta calidad que cubren todas sus necesidades.
+                </p>
+                <p class="text-justify large-text">
+                    Nuestro objetivo es proporcionar a nuestros clientes una plataforma segura y fácil de usar, donde
+                    puedan encontrar todo lo que buscan al mejor precio.
+                    Nos esforzamos por mantenernos actualizados con las últimas tendencias y asegurar la disponibilidad
+                    de los productos más demandados.
+                </p>
+                <p class="text-justify large-text">
+                    Valoramos la satisfacción de nuestros clientes y nos dedicamos a ofrecer un servicio al cliente
+                    excepcional.
+                    Gracias por elegirnos como su tienda virtual de confianza. Esperamos poder servirles y superar sus
+                    expectativas en cada compra.
+                </p>
+            </div>
+        </div>
+        <?php include ('include/pie_pagina.php'); ?>
+    </div>
+
+
+
+    <!-- jQuery  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/js/bootstrap.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
+
+    <!-- App js -->
+    <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
+
+</body>
+
+</html>
