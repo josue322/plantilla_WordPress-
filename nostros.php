afae00fadd6467e2a9792008cd64bdfd76c6cfb9
Josue López <josuexd123lc@gmail.com>
2024-05-16 08:36:33 -0500

    update

--- nostros.php
+++ nostros.php
@@ -22,7 +22,6 @@
 
         .large-text {
             font-size: 1.2rem;
-            /* Ajusta este valor según tus necesidades */
         }
     </style>
 </head>
@@ -30,35 +29,40 @@
 <body>
     <?php include ('include/menu.php'); ?>
     <br><br><br>
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-md-4 d-flex justify-content-center align-items-center">
-                <img src="imagenes/33926398-personal-del-equipo-de-ventas-sonriente-en-supermercado-con-los-brazos-cruzados (1).jpg"
-                    class="img-fluid" alt="Imagen de Nosotros">
-            </div>
-            <div class="col-md-8">
-                <h2 class="text-center">Sobre Nosotros</h2>
-                <p class="text-justify large-text">
-                    Bienvenidos a nuestra tienda virtual. Somos una empresa comprometida con ofrecer una experiencia de
-                    compra en línea excepcional.
-                    Contamos con un catálogo extenso de productos de alta calidad que cubren todas sus necesidades.
-                </p>
-                <p class="text-justify large-text">
-                    Nuestro objetivo es proporcionar a nuestros clientes una plataforma segura y fácil de usar, donde
-                    puedan encontrar todo lo que buscan al mejor precio.
-                    Nos esforzamos por mantenernos actualizados con las últimas tendencias y asegurar la disponibilidad
-                    de los productos más demandados.
-                </p>
-                <p class="text-justify large-text">
-                    Valoramos la satisfacción de nuestros clientes y nos dedicamos a ofrecer un servicio al cliente
-                    excepcional.
-                    Gracias por elegirnos como su tienda virtual de confianza. Esperamos poder servirles y superar sus
-                    expectativas en cada compra.
-                </p>
+    <div class="container my-5 d-flex justify-content-center">
+        <div class="card" style="width: 100%; max-width: 900px;">
+            <div class="row g-0">
+                <div class="col-md-4 d-flex justify-content-center align-items-center p-3">
+                    <img src="imagenes/33926398-personal-del-equipo-de-ventas-sonriente-en-supermercado-con-los-brazos-cruzados (1).jpg"
+                        class="img-fluid" alt="Imagen de Nosotros">
+                </div>
+                <div class="col-md-8 p-3">
+                    <h2 class="text-center">Sobre Nosotros</h2>
+                    <p class="text-justify large-text">
+                        Bienvenidos a nuestra tienda virtual. Somos una empresa comprometida con ofrecer una experiencia
+                        de
+                        compra en línea excepcional. Contamos con un catálogo extenso de productos de alta calidad que
+                        cubren todas sus necesidades.
+                    </p>
+                    <p class="text-justify large-text">
+                        Nuestro objetivo es proporcionar a nuestros clientes una plataforma segura y fácil de usar,
+                        donde
+                        puedan encontrar todo lo que buscan al mejor precio. Nos esforzamos por mantenernos actualizados
+                        con las últimas tendencias y asegurar la disponibilidad
+                        de los productos más demandados.
+                    </p>
+                    <p class="text-justify large-text">
+                        Valoramos la satisfacción de nuestros clientes y nos dedicamos a ofrecer un servicio al cliente
+                        excepcional. Gracias por elegirnos como su tienda virtual de confianza. Esperamos poder
+                        servirles y superar sus
+                        expectativas en cada compra.
+                    </p>
+                </div>
             </div>
         </div>
-        <?php include ('include/pie_pagina.php'); ?>
     </div>
+
+    <?php include ('include/pie_pagina.php'); ?>
 
     <!-- jQuery  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/js/bootstrap.bundle.min.js"></script>
